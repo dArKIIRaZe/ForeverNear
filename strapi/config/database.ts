e563c3bd8c13a2d5e5b52eeb2c53f8303c794052
dArKIIRaZe <63492184+dArKIIRaZe@users.noreply.github.com>
2025-06-07 17:29:39 +0100

    AWS Fix

--- strapi/config/database.ts
+++ strapi/config/database.ts
@@ -2,13 +2,10 @@ import path from 'path';
 
 export default ({ env }) => ({
   connection: {
-    client: 'postgres',
+    client: 'sqlite',
     connection: {
-      connectionString: env('DATABASE_URL', 'postgresql://foreverneardb_user:UIIBvbOl0LyLoyy6d0cqlkCh11NUcGaT@dpg-d125bi49c44c73fvdg40-a/foreverneardb'),
-      ssl: env.bool('DATABASE_SSL', false) && {
-        rejectUnauthorized: false,
-      },
+      filename: path.join(__dirname, '..', '..', env('DATABASE_FILENAME', '.tmp/data.db')),
     },
-    acquireConnectionTimeout: env.int('DATABASE_CONNECTION_TIMEOUT', 60000),
+    useNullAsDefault: true,
   },
 });
--- strapi/config/plugins.ts
+++ strapi/config/plugins.ts
@@ -4,7 +4,7 @@ export default ({ env }) => ({
       provider: 'aws-s3',
       providerOptions: {
         accessKeyId: env('AWS_ACCESS_KEY_ID'),
-        secretAccessKey: env('AWS_ACCESS_SECRET'),
+        secretAccessKey: env('AWS_SECRET_ACCESS_KEY'),
         region: env('AWS_REGION'),
         params: {
           Bucket: env('AWS_BUCKET'),
