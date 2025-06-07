e998b1b32a50093e3421a60163283db30cd9a579
dArKIIRaZe <63492184+dArKIIRaZe@users.noreply.github.com>
2025-06-07 17:35:34 +0100

    Update database.ts

--- strapi/config/database.ts
+++ strapi/config/database.ts
@@ -1,11 +1,28 @@
 import path from 'path';
 
-export default ({ env }) => ({
-  connection: {
-    client: 'sqlite',
-    connection: {
-      filename: path.join(__dirname, '..', '..', env('DATABASE_FILENAME', '.tmp/data.db')),
-    },
-    useNullAsDefault: true,
-  },
-});
+export default ({ env }) => {
+  const isProduction = env('NODE_ENV') === 'production';
+
+  if (isProduction) {
+    return {
+      connection: {
+        client: 'postgres',
+        connection: {
+          connectionString: env('DATABASE_URL'),
+          ssl: { rejectUnauthorized: false },
+        },
+        acquireConnectionTimeout: env.int('DATABASE_CONNECTION_TIMEOUT', 60000),
+      },
+    };
+  } else {
+    return {
+      connection: {
+        client: 'sqlite',
+        connection: {
+          filename: path.join(__dirname, '..', '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+        },
+        useNullAsDefault: true,
+      },
+    };
+  }
+};
