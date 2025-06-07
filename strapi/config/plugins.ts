5332787fd0330838bed2838c44a45237aab0f878
dArKIIRaZe <63492184+dArKIIRaZe@users.noreply.github.com>
2025-06-07 15:30:57 +0100

    AWS 3S Added

--- strapi/config/plugins.ts
+++ strapi/config/plugins.ts
@@ -1,1 +1,19 @@
-export default () => ({});
+export default ({ env }) => ({
+  upload: {
+    config: {
+      provider: 'aws-s3',
+      providerOptions: {
+        accessKeyId: env('AWS_ACCESS_KEY_ID'),
+        secretAccessKey: env('AWS_ACCESS_SECRET'),
+        region: env('AWS_REGION'),
+        params: {
+          Bucket: env('AWS_BUCKET'),
+        },
+      },
+    },
+  },
+});
+AWS_ACCESS_KEY_ID=your-access-key-id
+AWS_ACCESS_SECRET=your-secret-access-key
+AWS_REGION=your-bucket-region
+AWS_BUCKET=your-bucket-name
