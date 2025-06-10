c1d1ce18566ad5e6f61bb0b2a7faf330f25f8093
harley <harleyswork@live.co.uk>
2025-06-10 21:39:20 +0100

    undo

--- strapi/src/api/user-video/controllers/user-video.ts
+++ strapi/src/api/user-video/controllers/user-video.ts
@@ -1,22 +1,7 @@
-// src/api/user-videos/controllers/user-videos.ts
+/**
+ * user-video controller
+ */
 
-import { factories } from '@strapi/strapi';
+import { factories } from '@strapi/strapi'
 
-export default factories.createCoreController('api::user-videos.user-videos', ({ strapi }) => ({
-  async create(ctx) {
-    const user = ctx.state.user;
-
-    if (!user) {
-      return ctx.unauthorized('You must be logged in to upload.');
-    }
-
-    ctx.request.body.data = {
-      ...ctx.request.body.data,
-      user: user.id,         // ✅ correct relational ID
-      user_email: user.email // ✅ optional plain-text email field
-    };
-
-    const response = await super.create(ctx);
-    return response;
-  },
-}));
+export default factories.createCoreController('api::user-video.user-video');
