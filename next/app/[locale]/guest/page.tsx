9350430665556e8a9aecc7f715f18d37ad40def6
harley <harleyswork@live.co.uk>
2025-06-23 22:34:41 +0100

    commented out lost and found link because it doesn't do anythijng yet anyway so no vuln

--- next/app/[locale]/guest/page.tsx
+++ next/app/[locale]/guest/page.tsx
@@ -109,11 +109,12 @@ export default function GuestPage() {
               </button>
             </form>
 
-            <div className="text-center">
+           {<!-- <div className="text-center">
               <button className="text-blue-600 underline text-sm">
                 I don&apos;t know the answers (Lost &amp; Found)
               </button>
             </div>
+            --!}
 
             {submitted && error && (
               <p className="text-red-600 mt-4 text-sm">{error}</p>
